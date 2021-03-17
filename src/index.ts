c11b17505350bd37a743476ef07b4fec028f81b9
SungHun Moon <seunghunmoon023@gene1on1.com>
2021-03-17 21:09:56 +0900

    type: feat body : http -> https server porting
    See also : #SSL Certification Issue https://m-falcon.tistory.com/436

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,57 @@
-import Koa, {Request, Response, Next} from 'koa';
+import Koa, {Context, Request, Response, Next} from 'koa';
 import Router from '@koa/router';
 import WebSocket from "ws";
-import WebSocketConfig from '../config/config.json';
-import http from "http";
+import cors from "@koa/cors";
+import config from '../config/config.json';
+import corsOptions from '../config/corsOptions.json';
+import sslConfig from '../config/sslConfig.json';
+import https from "https";
+import path from 'path';
+import * as fs from "fs";
+import {IncomingMessage} from "http";
 
 const app = new Koa();
 const router = new Router();
-//   type RequestListener = (req: IncomingMessage, res: ServerResponse) => void;
-WebSocketConfig.server = http.createServer({request: Request, response: Response});
 
-const wss = new WebSocket.Server()
+const homeDirectory : string = process.env['HOME']!.toString();
+const sslKeyPath : string = path.join(homeDirectory, '.ssh','ssl_key');
+const [privateKeyFile, publicCRTFile] = [path.join(sslKeyPath, sslConfig.privateKeyFileName), path.join(sslKeyPath, sslConfig.publicCRTFileName)];
 
-console.dir(WebSocketConfig);
+const sslOptions = {
+    key: fs.readFileSync(privateKeyFile, 'utf8'),
+    cert: fs.readFileSync(publicCRTFile, 'utf8')
+};
+
+app.use(cors(
+    {
+        origin: (ctx: Context)=>{
+            return '*';
+        },
+        ...corsOptions
+    }
+));
+
+app.on('error', (error: Error, ctx: Context)=> {
+    console.error(`server error ${error} ${ctx}`);
+});
+
+router.get('default', '/', async (ctx: Context, next: Next)=>{
+    await next();
+    ctx.response.type = 'json';
+    ctx.response.body = 'this message is from https Server';
+});
+
+app.use(router.routes());
+
+// https://localhost:port 로 연결해보자.
+const httpsServer = https.createServer(sslOptions, app.callback());
+
+httpsServer.listen(config.port, ()=>{
+    console.log(`Server is running on ${config.port}`);
+})
+
+
+const wss = new WebSocket.Server({...config, server : httpsServer})
 
 
 function broadCast(message: string) {
@@ -28,7 +68,6 @@ function connection(webSocket : WebSocket, request: Request) {
     const clientIp : string = request.socket.remoteAddress!;
     webSocket.send(`hello, your ip is ${clientIp}`);
 }
-
 
 wss.on('connection', connection)
 
