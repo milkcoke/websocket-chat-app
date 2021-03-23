12a64add10034c9cf884e4a210c23fc0f37a9b10
SungHun Moon <seunghunmoon023@gene1on1.com>
2021-03-23 14:39:09 +0900

    refactor : WebSocket Initialize in module
    Resolves: #import module in HTML type="module"

--- src/client.ts
+++ src/client.ts
@@ -1,19 +1,30 @@
 // import config from '../config/config.json'
-let webSocket = new WebSocket('wss://localhost:3000');
 
-webSocket.onopen = ()=>{
-    console.log('web socket client connect with server!');
+export default function initializeWebSocket() {
+    const webSocket = new WebSocket('wss://localhost:3000');
+
+    webSocket.onopen = ()=>{
+        console.log('web socket client connect with server!');
+    }
+
+    webSocket.onerror = (error)=>{
+        console.error(error);
+    }
+
+    webSocket.onclose = ()=>{
+        console.log('web socket client disconnect with server!');
+        console.log('disconnected time : ', new Date());
+    }
+    webSocket.onmessage = (message) => {
+        console.log(`from server message: ${message.data}`);
+    }
+
+    // ping 받으면 console 에 표시ㅋ
+    // 이건 또 작동 안함. (client 에서 이벤트가 ping 이란걸 인지 못함)
+    webSocket.addEventListener('ping', ()=>{
+        console.log(`client received ping from server!`);
+    })
+
+    return webSocket;
 }
 
-webSocket.onerror = (error)=>{
-    console.error(error);
-}
-
-webSocket.onclose = ()=>{
-    console.log('web socket client disconnect with server!');
-}
-webSocket.onmessage = (message) => {
-    console.log(`from server message: ${message.data}`);
-}
-
-export default webSocket;
