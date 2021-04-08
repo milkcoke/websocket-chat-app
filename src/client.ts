7265a31e11269ea59780a08e615374b306709f13
SungHun Moon <seunghunmoon023@gene1on1.com>
2021-04-08 17:42:23 +0900

    feat: websocket auto reconnection with duration
    default reconnect duration is 4 seconds increasing doubly by 32 seconds. If over 1 minute, not trying to reconnect WebSocket anymore

--- src/client.ts
+++ src/client.ts
@@ -1,20 +1,37 @@
-// import config from '../config/config.json'
+// import {path, port, reconnectDuration} from '../config/config.json'
 
-export default function initializeWebSocket() {
-    const webSocket = new WebSocket('wss://localhost:3000');
+export default async function initializeWebSocket(duration: number = 4) {
+
+    if (duration > 60) {
+        throw new Error("Failed to connect websocket!");
+    }
+
+    let webSocket = new WebSocket('wss://localhost:4000');
 
     webSocket.onopen = ()=>{
         console.log('web socket client connect with server!');
+        // 열리면 반환
+        return webSocket;
     }
 
     webSocket.onerror = (error)=>{
         console.error(error);
     }
 
-    webSocket.onclose = ()=>{
+    webSocket.onclose = (closeEvent)=>{
         console.log('web socket client disconnect with server!');
         console.log('disconnected time : ', new Date());
+
+        // 연결 실패시 시간을 2배 더해 재연결 시도
+        if (closeEvent.code !== 1000) {
+            console.log(`close code : ${closeEvent.code} reason: ${closeEvent.reason}`);
+            setTimeout(()=>{
+                initializeWebSocket(duration * 2);
+            }, duration * 1000);
+        }
+
     }
+
     webSocket.onmessage = (message) => {
         console.log(`from server message: ${message.data}`);
     }
@@ -23,8 +40,9 @@ export default function initializeWebSocket() {
     // 이건 또 작동 안함. (client 에서 이벤트가 ping 이란걸 인지 못함)
     webSocket.addEventListener('ping', ()=>{
         console.log(`client received ping from server!`);
-    })
+    });
 
-    return webSocket;
 }
+
+
 
